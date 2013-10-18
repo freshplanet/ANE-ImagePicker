1cf14d9b2081dc00834599c7531704eae4cc18c3
Arnaud <arnaud@freshplanet.com>
2013-10-18 12:01:04 -0400

    renamed base activity class & made it abstract

--- android/src/com/freshplanet/ane/AirImagePicker/ImagePickerActivityBase.java
+++ android/src/com/freshplanet/ane/AirImagePicker/ImagePickerActivityBase.java
@@ -1,12 +1,23 @@
 package com.freshplanet.ane.AirImagePicker;
 
+import java.io.File;
+
+import com.freshplanet.ane.AirImagePicker.AirImagePickerUtils.SavedBitmap;
+
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images;
+import android.provider.MediaStore.Images.Media;
 import android.util.Log;
 
-public class AirImagePickerActivity extends Activity
+public abstract class ImagePickerActivityBase extends Activity
 {
 	public static final String TAG = "AirImagePicker";
 	
@@ -44,48 +55,59 @@ public class AirImagePickerActivity extends Activity
 		
 		super.onCreate(savedInstanceState);
 
-		if(AirImagePickerExtension.context != null) {
-			Log.d(TAG, "[AirImagePickerActivity] extension context exists");
-			AirImagePickerExtension.context.onCreatePickerActivity(this);
-		} else {
-			Log.e(TAG, "[AirImagePickerActivity] extension context is NULL !!");
+		if(AirImagePickerExtension.context == null) {
+			Log.e(TAG, "[AirImagePickerActivity] extension context has died");
 		}
+		
+		
 		Log.d(TAG, "[AirImagePickerActivity] Exiting onCreate");
 	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if(resultCode == RESULT_CANCELED) {
+			this.finish();
+		} else {
+			handleResult(data);
+		}
+	}
+	
+	protected abstract void handleResult(Intent data);
 	
 	protected AirImagePickerExtensionContext getExtensionContext() {
 		return AirImagePickerExtension.context;
 	}
 	
 	
-	protected Boolean sendResultToContext(String code) 
+	protected void sendResultToContext(String code) 
 	{
-		return sendResultToContext(code, null);
+		sendResultToContext(code, null);
 	}
 	
-	protected Boolean sendResultToContext(String code, String level)
+	protected void sendResultToContext(String code, String level)
 	{
-		if(AirImagePickerExtension.context != null) {
-			//TODO transfer properties of "result" to context here
-			AirImagePickerExtension.context.dispatchResultEvent(code, level);
-			return true;
+		if(AirImagePickerExtension.context == null) {
+			restartApp();
+		} else if (result.getPickedImage() != null) {
+			AirImagePickerExtensionContext context = AirImagePickerExtension.context;
+			context.setImagePath(result.imagePath);
+			context.setVideoPath(result.videoPath);
+			context.setPickedImage(result.getPickedImage());
+			context.dispatchResultEvent(code, level);
 		}
-		return false;
+		finish();
 	}
 	
-	protected void setFieldsOnContext() 
-	{
-		if(AirImagePickerExtension.context != null) {
-			
-		}
-	}
 	
 	protected void restartApp()
 	{
 		Intent launchIntent = this.getPackageManager().getLaunchIntentForPackage(airPackageName);
 		if(launchIntent != null) {
 			launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			launchIntent.setData(result.toUri());
+			if(result.getPickedImage() != null) {
+				launchIntent.setData(result.toUri());
+			}
 	        startActivity(launchIntent);
 	        finish();
 		} else {
@@ -137,5 +159,43 @@ public class AirImagePickerActivity extends Activity
 			startActivity(intent);
     	}
     }
+
+	protected SavedBitmap orientAndSaveImage(String filePath, int maxWidth, int maxHeight, String albumName )
+	{
+		Log.d(AirImagePickerUtils.TAG, "[AirImagePickerExtensionContext] Entering processPickedImage");
+		
+	
+		Bitmap image = AirImagePickerUtils.getOrientedSampleBitmapFromPath(filePath);
+	
+		if(albumName != null) {
+			File savedPicture = AirImagePickerUtils.savePictureInGallery(albumName, AirImagePickerUtils.getJPEGRepresentationFromBitmap(image));
+			if(savedPicture != null) {
+				notifyGalleryOfNewImage(savedPicture);
+			}
+		}
+		
+		image = AirImagePickerUtils.resizeImage(image, maxWidth, maxHeight);
+		String outputPath = AirImagePickerUtils.saveImageToTemporaryDirectory(image);
+		
+		Log.d(AirImagePickerUtils.TAG, "[AirImagePickerExtensionContext] Exiting processPickedImage");
+		
+		return new AirImagePickerUtils.SavedBitmap(image, outputPath);
+	}
+
+	protected void notifyGalleryOfNewImage(File picture)
+	{
+		long current = System.currentTimeMillis();
+		Log.d(AirImagePickerUtils.TAG, "[AirImagePickerExtensionContext] Entering notifyGalleryOfNewImage");
+		ContentValues values = new ContentValues();
+		values.put(MediaStore.Images.Media.TITLE, "My HelloPop Image " + current);
+		values.put(MediaStore.Images.Media.DATE_ADDED, (int) (current/1000));
+		values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+		values.put(MediaStore.Images.Media.DATA, picture.getAbsolutePath());
+		ContentResolver contentResolver = getContentResolver();
+		Uri base = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+		Uri newUri = contentResolver.insert(base, values);
+		sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, newUri));
+		Log.d(AirImagePickerUtils.TAG, "[AirImagePickerExtensionContext] Exiting notifyGalleryOfNewImage");
+	}
 	
 }
